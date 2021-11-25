d8a6b8b8574bf3456dc747905437ac7731cc3ab8
eminshamshoian <eminshamshoian@gmail.com>
2021-11-25 15:22:17 -0800

    added API search and implemented input for search in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 
 const Home = () => {
-  return <Layout>This is home</Layout>;
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const onInChange = e => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <Layout>
+      <input
+        type="text"
+        onChange={onInChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </Layout>
+  );
 };
 
 export default Home;
