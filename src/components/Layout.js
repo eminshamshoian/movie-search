6857b624f830468e271ebffc154fe5bb4a06a7fa
David <davidnickromero@gmail.com>
2021-12-14 01:03:37 -0800

    Styling for App Layout & Home

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Navs from './Navs';
 import Title from './Title';
 
+const styles = {
+  "padding": "5px",
+  "color":"white"
+}
+
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div style={styles}>
       <Title
         title="Box Office"
         subtitle="Are you looking for a movie or an actor?"
